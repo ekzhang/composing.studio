0b99399ebc41f03911905d14c6ffa3efe7f5c942
Vincent Huang <vincenthuang75025@yahoo.com>
2021-09-19 00:32:51 -0400

    Use synth controller

--- src/components/Score.tsx
+++ src/components/Score.tsx
@@ -1,101 +1,67 @@
 import abcjs from "abcjs";
-import { useEffect, useState } from "react";
-import {
-  Button,
-  Slider,
-  SliderTrack,
-  SliderFilledTrack,
-  SliderThumb,
-} from "@chakra-ui/react";
+import "abcjs/abcjs-audio.css";
+import { useEffect } from "react";
 
 type ScoreProps = {
   notes: string;
 };
 
-const myContext = new AudioContext();
 const synth = new abcjs.synth.CreateSynth();
+const synthControl = new abcjs.synth.SynthController();
+const cursorControl = {
+  beatSubdivisions: 2,
+  onStart: function () {
+    console.log("The tune has started playing.");
+  },
+  onFinished: function () {
+    console.log("The tune has stopped playing.");
+  },
+  onBeat: function (beatNumber: any) {
+    console.log("Beat " + beatNumber + " is happening.");
+  },
+  onEvent: function (event: any) {
+    console.log("An event is happening", event);
+  },
+};
 
 function Score({ notes }: ScoreProps) {
-  const [beginPlaying, setBeginPlaying] = useState(false);
-  const [tempo, setTempo] = useState(60);
+  useEffect(() => {
+    let visualObj = abcjs.renderAbc("paper", notes, {
+      responsive: "resize",
+      add_classes: true,
+    });
 
-  myContext.resume();
+    synth
+      .init({ visualObj: visualObj[0] })
+      .then(function () {
+        synthControl
+          .setTune(visualObj[0], false, { chordsOff: false })
+          .then(function () {
+            console.log("Audio successfully loaded.");
+          })
+          .catch(function (error: any) {
+            console.warn("Audio problem:", error);
+          });
+      })
+      .catch(function (error: any) {
+        console.warn("Audio problem:", error);
+      });
+  }, [notes]);
 
   useEffect(() => {
-    let visualObj = abcjs.renderAbc("paper", notes, {});
-
-    if (beginPlaying) {
-      // some funky logic bc we can't tell if a play is in progress
-      // should be fixed. the current issue is that
-      // if i remove the check on loaded, it begins processing audio
-      // as soon as the page loads, and then crashes
-      synth
-        .init({
-          audioContext: myContext,
-          visualObj: visualObj[0],
-          millisecondsPerMeasure: 240000 / tempo, // assuming 4/4 but whatever
-        })
-        .then(() => {
-          synth.prime().then(() => {
-            synth.start();
-          });
-        });
-    }
-  }, [notes, beginPlaying, tempo]);
+    synthControl.load("#audio", cursorControl, {
+      displayLoop: true,
+      displayRestart: true,
+      displayPlay: true,
+      displayProgress: true,
+      displayWarp: true,
+    });
+  }, []);
 
   return (
     <div>
+      <div id="audio"></div>
       <div id="paper"></div>
-      <Button
-        color="blue"
-        onClick={() => {
-          setBeginPlaying(true);
-        }}
-        variant="outline"
-      >
-        Play
-      </Button>
-      <Button
-        color="blue"
-        onClick={() => {
-          synth.pause();
-        }}
-        variant="outline"
-      >
-        Pause
-      </Button>
-      <Button
-        color="blue"
-        onClick={() => {
-          synth.resume();
-        }}
-        variant="outline"
-      >
-        Resume
-      </Button>
-      <Button
-        color="blue"
-        onClick={() => {
-          setBeginPlaying(false); // allows you to replay
-          synth.stop();
-        }}
-        variant="outline"
-      >
-        Reset
-      </Button>
-      <Slider
-        defaultValue={60}
-        min={20}
-        max={180}
-        aria-label="slider-ex-5"
-        onChangeEnd={(tempo) => setTempo(tempo)}
-      >
-        <SliderTrack>
-          <SliderFilledTrack />
-        </SliderTrack>
-        <SliderThumb />
-      </Slider>
-      <div>Current tempo: {tempo} bpm</div>
     </div>
   );
 }
