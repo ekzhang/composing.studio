cfe07da02d399098ffe45677d87d3216bf730605
Eric Zhang <ekzhang1@gmail.com>
2021-09-19 01:33:26 -0400

    Audio playback of generated scores (#15)
    * play and pause audio
    
    * Add explanation because i gtg
    
    * Add tempo slider
    
    * Fixed bpm computation
    
    * Move audiocontext and delete some code
    
    * Move synth creation outside
    
    * Use synth controller
    
    * Add live cursor with audio playback
    
    * Add basic styling to the score component
    
    Co-authored-by: Vincent Huang <vincenthuang75025@yahoo.com>

--- src/components/Score.tsx
+++ src/components/Score.tsx
@@ -1,100 +1,124 @@
+import { useEffect } from "react";
+import { Box, Stack } from "@chakra-ui/react";
 import abcjs from "abcjs";
-import { useEffect, useState } from "react";
-import {
-  Button,
-  Slider,
-  SliderTrack,
-  SliderFilledTrack,
-  SliderThumb,
-} from "@chakra-ui/react";
+import "abcjs/abcjs-audio.css";
+
+class CursorControl {
+  // This demonstrates two methods of indicating where the music is.
+  // 1) An element is created that is moved along for each note.
+  // 2) The currently being played note is given a class so that it can be transformed.
+  private cursor: SVGLineElement | null = null; // This is the svg element that will move with the music.
+
+  constructor(private readonly rootSelector: string) {}
+
+  onStart() {
+    // This is called when the timer starts so we know the svg has been drawn by now.
+    // Create the cursor and add it to the sheet music's svg.
+    const svg = document.querySelector(this.rootSelector + " svg");
+    this.cursor = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "line"
+    );
+    this.cursor.setAttribute("class", "abcjs-cursor");
+    this.cursor.setAttributeNS(null, "x1", "0");
+    this.cursor.setAttributeNS(null, "y1", "0");
+    this.cursor.setAttributeNS(null, "x2", "0");
+    this.cursor.setAttributeNS(null, "y2", "0");
+    svg?.appendChild(this.cursor);
+  }
+
+  removeSelection() {
+    // Unselect any previously selected notes.
+    var lastSelection = document.querySelectorAll(
+      this.rootSelector + " .abcjs-highlight"
+    );
+    for (var k = 0; k < lastSelection.length; k++)
+      lastSelection[k].classList.remove("abcjs-highlight");
+  }
+
+  onEvent(ev: any) {
+    // This is called every time a note or a rest is reached and contains the coordinates of it.
+    if (ev.measureStart && ev.left === null) return; // this was the second part of a tie across a measure line. Just ignore it.
+
+    this.removeSelection();
+
+    // Select the currently selected notes.
+    for (var i = 0; i < ev.elements.length; i++) {
+      var note = ev.elements[i];
+      for (var j = 0; j < note.length; j++) {
+        note[j].classList.add("abcjs-highlight");
+      }
+    }
+
+    // Move the cursor to the location of the current note.
+    if (this.cursor) {
+      this.cursor.setAttribute("x1", String(ev.left - 2));
+      this.cursor.setAttribute("x2", String(ev.left - 2));
+      this.cursor.setAttribute("y1", ev.top);
+      this.cursor.setAttribute("y2", ev.top + ev.height);
+    }
+  }
+
+  onFinished() {
+    this.removeSelection();
+
+    if (this.cursor) {
+      this.cursor.setAttribute("x1", "0");
+      this.cursor.setAttribute("x2", "0");
+      this.cursor.setAttribute("y1", "0");
+      this.cursor.setAttribute("y2", "0");
+    }
+  }
+}
+
+const synth = new abcjs.synth.CreateSynth();
+const synthControl = new abcjs.synth.SynthController();
+const cursorControl = new CursorControl("#paper");
 
 type ScoreProps = {
   notes: string;
 };
 
-const myContext = new AudioContext();
-const synth = new abcjs.synth.CreateSynth();
-
 function Score({ notes }: ScoreProps) {
-  const [beginPlaying, setBeginPlaying] = useState(false);
-  const [tempo, setTempo] = useState(60);
+  useEffect(() => {
+    synthControl.load("#audio", cursorControl, {
+      displayLoop: true,
+      displayRestart: true,
+      displayPlay: true,
+      displayProgress: true,
+      displayWarp: true,
+    });
+  }, []);
 
   useEffect(() => {
-    let visualObj = abcjs.renderAbc("paper", notes, { responsive: "resize" });
+    let visualObj = abcjs.renderAbc("paper", notes, {
+      responsive: "resize",
+      add_classes: true,
+    });
 
-    if (beginPlaying) {
-      // some funky logic bc we can't tell if a play is in progress
-      // should be fixed. the current issue is that
-      // if i remove the check on loaded, it begins processing audio
-      // as soon as the page loads, and then crashes
-      synth
-        .init({
-          audioContext: myContext,
-          visualObj: visualObj[0],
-          millisecondsPerMeasure: 240000 / tempo, // assuming 4/4 but whatever
-        })
-        .then(() => {
-          synth.prime().then(() => {
-            synth.start();
+    synth
+      .init({ visualObj: visualObj[0] })
+      .then(function () {
+        synthControl
+          .setTune(visualObj[0], false, { chordsOff: false })
+          .then(function () {
+            console.log("Audio successfully loaded.");
+          })
+          .catch(function (error: any) {
+            console.warn("Audio problem:", error);
           });
-        });
-    }
-  }, [notes, beginPlaying, tempo]);
+      })
+      .catch(function (error: any) {
+        console.warn("Audio problem:", error);
+      });
+  }, [notes]);
 
+  // TODO: Make this component reusable by generating random unique IDs.
   return (
-    <div>
-      <div id="paper"></div>
-      <Button
-        color="blue"
-        onClick={() => {
-          setBeginPlaying(true);
-        }}
-        variant="outline"
-      >
-        Play
-      </Button>
-      <Button
-        color="blue"
-        onClick={() => {
-          synth.pause();
-        }}
-        variant="outline"
-      >
-        Pause
-      </Button>
-      <Button
-        color="blue"
-        onClick={() => {
-          synth.resume();
-        }}
-        variant="outline"
-      >
-        Resume
-      </Button>
-      <Button
-        color="blue"
-        onClick={() => {
-          setBeginPlaying(false); // allows you to replay
-          synth.stop();
-        }}
-        variant="outline"
-      >
-        Reset
-      </Button>
-      <Slider
-        defaultValue={60}
-        min={20}
-        max={180}
-        aria-label="slider-ex-5"
-        onChangeEnd={(tempo) => setTempo(tempo)}
-      >
-        <SliderTrack>
-          <SliderFilledTrack />
-        </SliderTrack>
-        <SliderThumb />
-      </Slider>
-      <div>Current tempo: {tempo} bpm</div>
-    </div>
+    <Stack p={3}>
+      <Box id="paper" borderWidth="1px" borderColor="gray.500" rounded="sm" />
+      <Box id="audio" />
+    </Stack>
   );
 }
 
