18e5ab2120c996d4b7bf0b18d2e496ff932811d5
Eric Zhang <ekzhang1@gmail.com>
2021-09-18 22:26:59 -0400

    Make IDs human readable

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -2,16 +2,10 @@ import { useEffect, useState } from "react";
 import { Button, Flex, Image, Stack } from "@chakra-ui/react";
 import { VscArrowRight } from "react-icons/vsc";
 import { useHistory } from "react-router-dom";
-
-const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-const idLen = 6;
+import generate from "project-name-generator";
 
 function getRandomId() {
-  let id = "";
-  for (let i = 0; i < idLen; i++) {
-    id += chars[Math.floor(Math.random() * chars.length)];
-  }
-  return id;
+  return generate({ number: true }).dashed;
 }
 
 function LandingPage() {
